c5608ea8ce9e5bea4d0c196800f41f6281ca3e81
nowottnm <nowottnm@posteo.net>
2020-12-13 15:45:54 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO_N = 10
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(GPIO_N, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # pin 10 to input, init low
+button_message = "Button was pushed"
+import pdb; pdb.set_trace()
+while True:
+    if GPIO.input(GPIO_N) == GPIO.HIGH:
+        print(button_message)
+        #sleep(5)
+    else:
+        print (GPIO.input(GPIO_N))
