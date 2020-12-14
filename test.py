0d1c5f72b67d56ee70e73ed345503f7e4a4633ab
nowottnm <nowottnm@posteo.net>
2020-12-14 12:34:50 +0100

    working LED interface

--- test.py
+++ test.py
@@ -1,15 +1,29 @@
 import RPi.GPIO as GPIO
 from time import sleep
-GPIO_N = 12
+from gpiozero import Button, RGBLED
+from colorzero import color
+from random import random as rand
+GPIO.cleanup()
+GPIO_N = 8
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+GPIO.setup(36, GPIO.OUT)
+GPIO.setup(38, GPIO.OUT)
+GPIO.setup(40, GPIO.OUT)
+GPIO.output(36, GPIO.LOW)
+GPIO.output(38, GPIO.LOW)
+GPIO.output(40, GPIO.LOW)
 GPIO.setup(GPIO_N, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # pin 10 to input, init low
 button_message = "Button was pushed"
-import pdb; pdb.set_trace()
+#GPIO.cleanup()
+#led = RGBLED(red=36, green=38, blue=40)
+#button = Button(8)
 while True:
-    if GPIO.input(GPIO_N) == GPIO.HIGH:
-        print(button_message)
-        #sleep(5)
-        exit(0)
-    print(GPIO.input(GPIO_N))
+	if GPIO.input(GPIO_N) == GPIO.HIGH:
+		print(button_message)
+		GPIO.output(36, GPIO.HIGH)
+		GPIO.output(38, GPIO.HIGH)
+		GPIO.output(40, GPIO.HIGH)
+		sleep(1500)
+		#led.color=(0,0,0)
 
