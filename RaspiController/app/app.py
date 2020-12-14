547060c33780ec2afc992f33e23bc62fd1025681
maximilian.nowottnick <maximilian.nowottnick@supperundsupper.com>
2020-12-14 16:52:14 +0100

    added control script

--- RaspiController/app/app.py
+++ RaspiController/app/app.py
@@ -5,7 +5,11 @@ from colorzero import color
 from random import random as rand
 from RaspiController.message_parser.parser import EmailInterface as emailparser
 from RaspiController.Raspi_Gpio.gpio import RaspiGPIO as raspi
+import random
 import json
+
+def rnd():
+    return(random.random() > 0.5)
 
 def main():
     config = json.load(open(('~/Desktop/raspi_config.json')))
@@ -13,4 +17,13 @@ def main():
     connection = config['connection']
     rpi = raspi(**hardware)
     mail = emailparser(**connection)
-    while
+    rgb = [True, True, True]# init white
+    def button_callback(channel):
+        mail.send_message([rnd(), rnd(), rnd()])
+    GPIO.add_event_detect(rpi.button, GPIO.RISING,callback=button_callback)
+    while(True):
+        rgb_new = mail.receive_message()
+        if rgb_new:
+            rgb = rgb_new
+        rpi.led(rgb)
+        sleep(60)
