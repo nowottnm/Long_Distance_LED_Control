61a0ae6622dafa6410b7c75090708869760a191f
nowottnm <nowottnm@posteo.net>
2020-12-14 21:01:17 +0100

    working installer

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #git clone https://github.com/nowottnm/Long_Distance_LED_Control
 echo "alias python='/usr/bin/python3'" >> ~/.bashrc
 echo "export PATH=$HOME/.local/bin:$PATH" >> ~/.bashrc
-. ~/.bashrc
 pip3 install ~/Long_Distance_LED_Control/
 python3 ~/Long_Distance_LED_Control/install.py
-echo "sudo led_control&" >> /etc/profile
-
+echo "led_control&" >> ~/.bashrc
+. ~/.bashrc
