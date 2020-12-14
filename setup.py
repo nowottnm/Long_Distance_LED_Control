88f6d63c1c82247f7cd5cba5aca881003aea625c
maximilian.nowottnick <maximilian.nowottnick@supperundsupper.com>
2020-12-14 16:52:39 +0100

    added control script

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Long_Distance_LED_Control",
+    version="1.0",
+    author="Maximilian Nowottnick",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nowottnm/Long_Distance_LED_Control",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        "numpy"
+    ],
+    # entry_points = {
+    #     'console_scripts': ['geometry_export=geometry_export.runner.run_pipeline:main'],
+    # }
+)
