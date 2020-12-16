1537e9a0129727755a1b2eca69e1da67f3b7eaf6
nowottnm <nowottnm@posteo.net>
2020-12-16 19:10:14 +0100

    added three button script

--- test.py
+++ test.py
@@ -3,27 +3,30 @@ from time import sleep
 from gpiozero import Button, RGBLED
 from colorzero import color
 from random import random as rand
-GPIO.cleanup()
-GPIO_N = 8
-GPIO.setwarnings(False)
+GPIO.setwarnings(True)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(36, GPIO.OUT)
-GPIO.setup(38, GPIO.OUT)
-GPIO.setup(40, GPIO.OUT)
-GPIO.output(36, GPIO.LOW)
-GPIO.output(38, GPIO.LOW)
-GPIO.output(40, GPIO.LOW)
-GPIO.setup(GPIO_N, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # pin 10 to input, init low
+row = [31, 33, 35]
+coll =[37]
+for i in range(len(row)):
+	GPIO.setup(row[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for j in range(len(coll)):
+	GPIO.setup(coll[j], GPIO.OUT)
+	GPIO.output(coll[j],1)
 button_message = "Button was pushed"
 #GPIO.cleanup()
 #led = RGBLED(red=36, green=38, blue=40)
 #button = Button(8)
 while True:
-	if GPIO.input(GPIO_N) == GPIO.HIGH:
-		print(button_message)
-		GPIO.output(36, GPIO.HIGH)
-		GPIO.output(38, GPIO.HIGH)
-		GPIO.output(40, GPIO.HIGH)
-		sleep(1500)
-		#led.color=(0,0,0)
+	try:
+		for j in coll:
+			GPIO.output(j,0)
+			for i in row:
+				if GPIO.input(i)==0:
+					print((i,j))
+			GPIO.output(j,1)
+	except Exception as e:
+		print(e)
+		GPIO.cleanup()
+		exit(1)
+
 
