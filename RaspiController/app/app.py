e3933019593bfabb534c378e11006959761da959
maximilian.nowottnick <maximilian.nowottnick@supperundsupper.com>
2020-12-18 13:20:17 +0100

    restructured main loop to differ between cloud and local change

--- RaspiController/app/app.py
+++ RaspiController/app/app.py
@@ -11,7 +11,7 @@ from RaspiController.LED.led import LedInterface
 import random
 import json
 import time
-
+import copy
 
 def rnd():
     return(random.random() > 0.5)
@@ -25,19 +25,27 @@ def main():
     led = LedInterface(**led_config)
     parser = getattr(parsers, connection_config[0])(**connection_config[1])
     button = getattr(buttons, button_config[0])(**button_config[1])
-    rgb = parser.receive_message()
+    rgb_current = parser.receive_message()  # init rgb states from cloud
+    rgb_global = copy.deepcopy(rgb_current)
+    led.led(*rgb_current)
+    button_is_pressed = False
     start_time = time.time()
     while(True):
         try:
+            if button.get_input():
+                rgb_current = button.process_input(rgb_current)
+                button_is_pressed = True
             if (time.time() - start_time) > 90:
                 rgb_global = parser.receive_message()
+                if (rgb_global != rgb_current and )
+                    not button_is_pressed):   # if cloud is ahead but the own state was not changed by user ...
+                    rgb_current = rgb_global  # ... update from cloud
+                if button_is_pressed:         # if the button was pressed update cloud, reset button state
+                    parser.send_message(rgb_current)
+                    button_is_pressed = False
                 print("rgb in the cloud is: " + str(rgb_global))
-                start_time = time.time()
-                if rgb != rgb_global:
-                    parser.send_message(rgb)
-            if button.get_input():
-                rgb = button.process_input(rgb)
-            led.led(*rgb)
+                start_time = time.time()      # reset time since last check
+            led.led(*rgb_current)
         except KeyboardInterrupt:
             GPIO.cleanup()
             exit(1)
